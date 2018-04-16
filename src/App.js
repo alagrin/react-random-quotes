59b594dc2e13fa747a448ead594d8c689ee2e4aa
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-04-16 00:37:02 -0700

    Fixes and introduced new API call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
-import DisplayContent from './components/DisplayContent';
+import Button from './components/Button';
+import getQuote from "./quoteGrab";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-    };
+      quote: [],
+      randomQuote: 'Temporary quote'
+    }
+    this.handleRandomQuote = this.handleRandomQuote.bind(this);
   }
+  
+  componentDidMount() {
+    fetch('http://quotes.rest/qod.json', {
+    headers: {
+      "Accept": "application/json",
+      "X-TheySaidSo-Api-Secret": "OqhHWrDT_4T6zLqsQlq76AeF"
+    }
+  }).then(response => {
+    return response.json()
+  }).then(data => {
+    let quoteResponse = data.contents.quotes.map((randomQuote) => {
+      return (
+        <div key={randomQuote.id}>
+        <p>{randomQuote.title}</p>
+        <h4>{randomQuote.author}</h4>
+        <p>{randomQuote.quote}</p>
+        </div>
+      )
+    })
+    this.setState({
+      quote: quoteResponse
+    });
+  });
+}
 
-  render() {
-    return (
-      <div className="App">
-        <DisplayContent text="Your Quote of the Day" />
-      </div>
-    );
-  }
+handleRandomQuote() {
+  let result = getQuote();
+  // this.setState({
+  //   randomQuote: result
+  // });
+}
+
+render() {
+  return (
+    <div className="App card light-blue">
+      <h2>Your Quote of the Day</h2> 
+    <div className="row">
+      <Button buttonName="Twitter" onClick={this.handleRandomQuote}/>
+      <Button className="btn" buttonName="Facebook" />
+      <Button className="btn" buttonName="Quote" />
+    </div>
+    <div>{this.state.quote}</div>
+    <div>
+      <h3>Here's your random quote: </h3>
+      <p>{this.state.randomQuote}</p>
+    </div>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,10 @@
 import React from 'react';
 
-const buttonStyle = {
-    borderRadius:'50%',
-    height: '80px', 
-    width: '80px',
-    margin:'20px'
-};
-
 const Button = (props) => {
-    return (
-    <button style={buttonStyle}>
-            <a href="../public/index.html">{props.buttonName}</a>
-    </button>
-    );
+        return (
+            <div>
+                <a className="waves-effect waves-light btn" onClick={props.onClick}>{props.buttonName}</a>
+            </div>
+            );
 }
-
 export default Button;
-
-//a href will show props text or the particular function of the button
